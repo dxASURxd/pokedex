ed3e1b0578d2dc9cfe85189357500d294e69d103
asur <asur_xd@hotmail.com>
2024-06-01 12:38:58 -0600

    Update seed.service.ts

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -4,6 +4,7 @@ import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { decode } from 'punycode';
 
 @Injectable()
 export class SeedService {
@@ -17,16 +18,25 @@ export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>(' https://pokeapi.co/api/v2/pokemon?limit=650');
 
-    data.results.forEach(async ({ name, url }) => {
+    await this.pokemonModel.deleteMany({});
+
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=600');
+
+    const pokemonToInsert: { name: string, no: number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
 
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
 
-      const pokemon = await this.pokemonModel.create({name, no});
-      
-    })
+      // const pokemon = await this.pokemonModel.create({name, no});
+    
+      pokemonToInsert.push({name, no});
+    });
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
     return 'seed executed';
   }
 }
