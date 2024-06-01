073a5ddbbf1a8c08c1aa45c6bc062f5e7d02e0cd
asur <asur_xd@hotmail.com>
2024-06-01 11:27:58 -0600

    seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,31 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
-import { PokemonService } from 'src/pokemon/pokemon.service';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
 
- private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) { }
 
-  async executeSeed(){
-    const { data } = await this.axios.get<PokeResponse>(' https://pokeapi.co/api/v2/pokemon?limit=10');
 
-    data.results.forEach(({name, url}) => {
+  private readonly axios: AxiosInstance = axios;
 
-      // const segments = url.split('/');
-      // const no  = +segments[ segments.length -2 ];
-      // haciendo lo mismo pero de una forma mas eficiente, creamos una constante llamada 
-      // pokemonNo haciendo referencia al numero del pokemon
-      // esta variable es igual a la separación del url por sus diagonales
-      // seguido de acceder al valor numero -2 de la matriz ya dividida
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(' https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    data.results.forEach(async ({ name, url }) => {
+
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+
+      const pokemon = await this.pokemonModel.create({name, no});
       
-      const pokemonNo = +url.split('/').at(-2);
-      console.log(pokemonNo);
     })
-
-    return data.results;
-    
+    return 'seed executed';
   }
-
 }
