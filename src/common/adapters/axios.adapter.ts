c681bd4a2b34330440e4223764bdfc22547a3a26
asur <asur_xd@hotmail.com>
2024-06-08 09:09:08 -0600

    patrón adaptador
    un poco de documentación xd

--- src/common/adapters/axios.adapter.ts
+++ src/common/adapters/axios.adapter.ts
@@ -1,7 +1,11 @@
+// La idea de este envoltorio es que ayuda a que si axios cambia por alguna razon, solo tenemos
+// que cambiar la clase AxiosAdapter
 import axios, { AxiosInstance } from "axios";
 import { HttpAdapter } from "../interfaces/http-adapter.interface";
+import { Injectable } from "@nestjs/common";
 
-export class AxiosAdapter implements HttpAdapter{
+@Injectable()
+export class AxiosAdapter implements HttpAdapter{  // AxiosAdapter implementa la interfaz de mi HttpAdapter
     
     private axios: AxiosInstance = axios;
     
@@ -10,7 +14,24 @@ export class AxiosAdapter implements HttpAdapter{
             const { data } = await this.axios.get<T>(url);
             return data;
         } catch (error) {
-            throw new Error('error')
+            throw new Error('error');
         }
     }
 }
+
+
+
+
+// El Custom Provider es un patrón adaptador que encapsula el uso 
+// de un servicio externo para permitir cambios futuros sin alterar
+// el código principal. Se crea un adaptador personalizado para el
+// servicio externo, manteniendo una interfaz común. Este adaptador
+// sirve como una capa intermedia entre la aplicación y el servicio,
+// implementando una interfaz específica (HTTPAdapter) para la interacción.
+// La clase AxiosAdapter actúa como el adaptador específico para el
+// servicio, encapsulando las llamadas al mismo. Esto promueve la
+// modularidad y facilita la sustitución del servicio externo en el
+// futuro.
+
+// "interfaz" se refiere a un conjunto de métodos y propiedades que 
+// definen el comportamiento de una clase o componente
--- src/common/interfaces/http-adapter.interface.ts
+++ src/common/interfaces/http-adapter.interface.ts
@@ -2,3 +2,10 @@
 export interface HttpAdapter {
     get<T>( url: string ): Promise<T>
 }
+
+// Una clase adaptadora, declaramos lo que deba implementar
+// para poder utilizarla en cualquier otro servicio
+
+// Toca clase que implemente el HttpAdapter va a tener que tener
+// la peticion get, recibe el url (es de tipo String) y esto es una promesa
+// pero la promesa debe resolver algo
